4bfa971f3c4d1f5ffda81f4f4267936b3d1947b9
Devour0706 <abc122331a@gmail.com>
2025-05-16 19:21:34 +0800

    按下開始按鈕後隱藏開始菜單

--- Assets/Resources/MainSystem/AllGameManager.cs
+++ Assets/Resources/MainSystem/AllGameManager.cs
@@ -123,7 +123,7 @@ public class PlayerData
 public class LandData
 {
     public int Ignore = 0;//³WÁ×ÅÜ¼Æ
-    public Land[] lands = new Land[10];
+    public Land[] lands = new Land[37];
 }
 [SerializeField]
 public class Land
---
+++ Assets/Resources/MainSystem/UI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] Button StartButton;
+    [SerializeField] GameObject StartMenu;
+
+    public void HideStartMenu()
+    {
+        StartMenu.SetActive(false); //¡Ù¬√ StartMenu
+    }
+}
