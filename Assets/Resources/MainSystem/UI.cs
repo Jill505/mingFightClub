2a019e8a6d964e999d7357ffeef67f284bcff045
Devour0706 <abc122331a@gmail.com>
2025-05-16 20:07:05 +0800

    按下開始後才顯示台南地圖

--- Assets/Resources/MainSystem/UI.cs
+++ Assets/Resources/MainSystem/UI.cs
@@ -5,9 +5,11 @@ public class UI : MonoBehaviour
 {
     [SerializeField] Button StartButton;
     [SerializeField] GameObject StartMenu;
+    [SerializeField] GameObject TainanMap;
 
     public void HideStartMenu()
     {
         StartMenu.SetActive(false); //ÁôÂÃ StartMenu
+        TainanMap.SetActive(true); //ÁôÂÃ StartMenu
     }
 }
