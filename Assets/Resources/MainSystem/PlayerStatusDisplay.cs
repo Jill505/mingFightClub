56ea2a5a2eabb83dfdea0173e83c887688429566
Devour0706 <abc122331a@gmail.com>
2025-05-29 23:19:05 +0800

    增加顯示人口數量，並讓2級選單不會被玩家資訊覆蓋

--- Assets/Resources/MainSystem/PlayerStatusDisplay.cs
+++ Assets/Resources/MainSystem/PlayerStatusDisplay.cs
@@ -5,12 +5,13 @@ public class PlayerStatusDisplay : MonoBehaviour
 {
     public TextMeshProUGUI playerNameText;
     public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI populationText;
 
     private void Start()
     {
         AllGameManager allGameManager = GameObject.Find("AllGameManager")?.GetComponent<AllGameManager>();
         allGameManager.saveFile.playerData.playerSurname = "廖偉民";
-        allGameManager.saveFile.playerData.money = 100;
+        allGameManager.saveFile.playerData.InitializationPlayerData();
 
         if (allGameManager != null &&
             allGameManager.saveFile != null &&
@@ -19,11 +20,13 @@ public class PlayerStatusDisplay : MonoBehaviour
         {
             playerNameText.text = $"玩家姓名：{allGameManager.saveFile.playerData.playerSurname}";
             moneyText.text = $"金錢：{allGameManager.saveFile.playerData.money}元";
+            populationText.text = $"人口數：\n{allGameManager.saveFile.playerData.population}";
         }
         else
         {
             playerNameText.text = "玩家姓名：未知";
             moneyText.text = "金錢：未知";
+            populationText.text = $"人口數：未知";
         }
     }
 
@@ -36,10 +39,9 @@ public class PlayerStatusDisplay : MonoBehaviour
             allGameManager.saveFile.safeFile != null &&
             allGameManager.saveFile.safeFile.playerData != null)
         {
-            var playerData = allGameManager.saveFile.safeFile.playerData;
-
             playerNameText.text = $"玩家姓名：{allGameManager.saveFile.playerData.playerSurname}";
             moneyText.text = $"金錢：{allGameManager.saveFile.playerData.money}元";
+            populationText.text = $"人口數：\n{allGameManager.saveFile.playerData.population}";
         }
     }
 }
